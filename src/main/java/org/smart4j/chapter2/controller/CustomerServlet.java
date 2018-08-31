72e89a9208caba135c2bb3a30ecfd2c4e783db91
autumn <qy814379857@foxmail.com>
2018-08-31 22:34:02 +0800

    实现获取列表

--- src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
+++ src/main/java/org/smart4j/chapter2/controller/CustomerServlet.java
@@ -2,11 +2,34 @@ package org.smart4j.chapter2.controller;/**
  * Created by Administrator on 2018/8/12.
  */
 
+import com.smart4j.chapter2.model.Customer;
+import com.smart4j.chapter2.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
 /**
- * @program: chapter2->CustomerCreateServlet
- * @description: 新增customer
- * @author: qiuyu
- * @create: 2018-08-12 23:17
+ * @description: 进入客户列表界面
  **/
-public class CustomerServlet {
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet{
+    private CustomerService service;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        service = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = service.getCustomerList("");
+        req.setAttribute("customerList",customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req,resp);
+    }
 }
